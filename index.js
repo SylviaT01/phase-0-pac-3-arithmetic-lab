2fac1009d7c667f63382e1fae8de79260c794b57
Sylvia <sylviachebet03@gmail.com>
2024-03-12 18:50:20 +0300

    add

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(){
+     1 + 80;    
+}
+function subtract(){
+    60 - 40;    
+}
+function multiply(){
+    2 * 3.4;   
+}
+function divide(){
+    5.0 / 2.5;   
+}
+function add() {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+  function increment(){
+    return (a += 1);
+  }
+  function increment(a){
+    return a + 1 ;
+  }
+  function decrement(a){
+    return a - 1;
+  }
+  function makeInt(a){
+    return a
+  }
+  function makeInt(a){
+    return parseInt(a,10);
+  }
+  function preserveDecimal(a){
+    return parseFloat(a)
+  }
